583f8499fb3b565783db60b89cd3ddbd9bdb725a
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-04-13 13:37:36 +0200

    Commit initial

---
+++ src/tpcollections/TPCollections.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpcollections;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author diginamic05
+ */
+public class TPCollections {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ArrayList<User> users = new ArrayList();
+        User u = new User();
+        users.add(u);
+    }
+    
+}
---
+++ src/tpcollections/User.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpcollections;
+
+/**
+ *
+ * @author diginamic05
+ */
+public class User {
+    private String name;
+    private Integer age;
+    private boolean isMale;
+    
+    public User()
+    {
+        this.name = "";
+        this.age = 0;
+        this.isMale = false;
+    }
+    
+    public User(String n, Integer a, boolean ism)
+    {
+        this.age = a;
+        this.name = n;
+        this.isMale = ism;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the age
+     */
+    public Integer getAge() {
+        return age;
+    }
+
+    /**
+     * @return the isMale
+     */
+    public boolean isIsMale() {
+        return isMale;
+    }
+}
